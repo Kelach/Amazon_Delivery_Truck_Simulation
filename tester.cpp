3401effeb7f5b07d4bcb1d2c8b8e787dbe23103d
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-15 22:48:12 -0600

    renamed "address." to "tester.cpp" w/some code

---
+++ tester.cpp
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Function testing File for Traveling Salesman Project
+*/
+
+#include "traveling_salesman.h" // imports TSP header file
+#include <iostream>             // imports standard library thingies
+using namespace TravelingSalesman; // we may not want to keep this
+
+int main(){
+
+    Address addy1(1, 2, 10), addy2(4, 3, 13);
+    std::cout << "Distance: " << addy1.distance(addy2) << std::endl;
+    
+    return 0;
+}
