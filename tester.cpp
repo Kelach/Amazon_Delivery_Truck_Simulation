617909fe9ed8fedfa2be6d1fec994b3fac75fad2
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-16 11:52:27 -0600

    testing w/ tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -9,7 +9,9 @@ using namespace TravelingSalesman; // we may not want to keep this
 
 int main(){
 
-    // Address addy1(1, 2, 10), addy2(4, 3, 13);
+    
+    Address addy1(1, 2, 10);
+    // Address addy2(4, 3, 13);
     // std::cout << "Distance: " << addy1.distance(addy2) << std::endl;
     return 0;
 }
