8f3a62b8f7d2345ada2e632fccc75635f6aafdbe
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-17 14:17:12 -0600

    minor changes to TS.cpp

--- traveling_salesman.cpp
+++ traveling_salesman.cpp
@@ -7,21 +7,28 @@
 #include <iostream>  
 #include "traveling_salesman.h"           
 #include <math.h>
+#include <vector>
 using namespace TravelingSalesman;
 
-Address::Address(int i, int j, int deliver_by) : i(i), j(j), deliver_by(deliver_by) {};
 
+// Address Class Methods
+Address::Address(int i, int j, int deliver_by) : i(i), j(j), deliver_by(deliver_by) {};
 Address::~Address(){};
 
 double Address::distance(const Address& other){
-    // Cartesian distance
+    //Cartesian distance
     return sqrt( (i-other.i)*(i - other.i) + (j-other.j)*(j-other.j) ) ;
     
     //Manhattan distance
     //return abs(i-other.i) + abs(j-other.j);
 };
+// End of Address Class Methods
 
+// AddressList Class Methods
+AddressList::AddressList(){};
+AddressList::AddressList(std::vector<Address> address_list) : address_list(address_list){};
 
+// End of AddressList Class Methods
 
 
 
