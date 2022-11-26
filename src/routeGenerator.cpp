7ee8d22c5b87b336100cd69fb1cd4d8d8afe7b3e
Kelach <93161153+Kelach@users.noreply.github.com>
2022-11-25 23:48:49 -0600

    continued development of routeGenerator

--- src/routeGenerator.cpp
+++ src/routeGenerator.cpp
@@ -16,15 +16,8 @@ bool is_prime(int num){
   }
   return true;
 }
-
-class primegenerator{
-private:
-  int cur_prime;
-public:
-  primegenerator()
-    : cur_prime(2) {};
-  int nextprime(){
-    int prime_num = cur_prime;
+int nextprime(int cur_prime){
+  int prime_num = cur_prime;
     // if current prime is 2, set to 3
     if (cur_prime == 2){
       cur_prime = 3;
@@ -33,39 +26,37 @@ public:
       // and return next prime number
       int next_prime = cur_prime + 2;
       while(is_prime(next_prime) == false){
-	    next_prime += 2;
+        next_prime += 2;
       }
       cur_prime = next_prime;
     } 
     
     return prime_num;
   }
-  std::vector<int> generate_primes(int max_num){
-    // initalize an empty vector, 
-    // find every nextprime that is less
-    // than or equal to our max and add to our
-    // vector of primes
-    std::vector<int> primes = {};
-    int prime_num = 2;
-    while (max_num > prime_num){
-        primes.push_back(prime_num);
-        prime_num = nextprime();
-    }
-    // return vector of primes
-    // and reset current prime back to 2
-    cur_prime = 2;
-    return primes;
+std::vector<int> generate_primes(int max_num){
+  // initalize an empty vector, 
+  // find every nextprime that is less
+  // than or equal to our max and add to our
+  // vector of primes
+  std::vector<int> primes = {};
+  int prime_num = 2;
+  while (max_num >= prime_num){
+      primes.push_back(prime_num);
+      prime_num = nextprime(prime_num);
   }
-};
+  // return vector of primes
+  return primes;
+}
 
 
-std::random_device rd; //obtain a random number hardware
-std::mt19937 gen(rd()); // seed the generator
+  std::random_device rd; //obtain a random number hardware
+  std::mt19937 gen(rd()); // seed the generator
 
 /**
  * @brief generates random integer between low-high (inclusive)
  * */ 
 int random(int low, int high){
+
     std::uniform_int_distribution<> dist(low, high); // define range
     return dist(gen); // generats random number
 }
@@ -85,10 +76,11 @@ TravelingSalesman::Address generateAddress(std::string& keyword, int& space, std
             int j = random(0, space);
             int i = random(0, space);
         }
-        return TravelingSalesman::Address(i, j, delivery_by);
         // get 2 random ints within range of space
         // and one random int between 1 and 7 for delivery date
         // return Address object
+        return TravelingSalesman::Address(i, j, delivery_by);
+
 
     } else if (keyword == "primes"){
         // generate random i and j indicies
@@ -107,6 +99,41 @@ TravelingSalesman::Address generateAddress(std::string& keyword, int& space, std
         throw(5);
     }
 }
+/**
+ * @brief generates route with a starting hub at (0, 0)
+ * @param keyword generates route with "random" or "primes" coordinate points Addresses
+ * @param space defines space of x-y coordinate system.
+ * @param address_count # of addresses to generate per route
+ * */ 
+TravelingSalesman::Route generateRoute(std::string& keyword, int& space, int& address_count){
+  if (keyword == "random"){
+    std::vector<TravelingSalesman::Address> address_vec{};
+    for (int i=0;i<=address_count;i++){
+      std::vector<int> primes{};
+      // create address and add to vector address vec
+      TravelingSalesman::Address new_address = generateAddress(keyword, space, primes);
+      address_vec.push_back(new_address);
+    }
+    return TravelingSalesman::Route(address_vec, TravelingSalesman::Address(0, 0, 0));
+  } else if (keyword == "primes"){
+    std::vector<TravelingSalesman::Address> address_vec{};
+    // generate primes vector
+    for (int i=0;i<=address_count;i++){
+      // generate vector of primes
+      std::vector<int> primes = generate_primes(space);
+
+      // create random addresse co-ords based off of prime vector
+      TravelingSalesman::Address new_address = generateAddress(keyword, space, primes);
+      address_vec.push_back(new_address);
+    }
+    return TravelingSalesman::Route(address_vec, TravelingSalesman::Address(0, 0, 0));
+
+  } else{
+      std::vector<TravelingSalesman::Address> address_vec{};
+      throw(5);
+    return TravelingSalesman::Route(address_vec, TravelingSalesman::Address(0, 0, 0));
+  }
+}
 
 
 int main(int argc, char **argv){
@@ -114,14 +141,20 @@ int main(int argc, char **argv){
     
     // -help option
     cxxopts::Options options
-        ("cxxopts", "Commandline options demo");
+        ("cxxopts", "Commandline options");
     options.add_options()
         ("h,help","usage information");
     
     // # of routes option
     options.add_options()
-        ("n,n_routes","# of routes to generate",
-        cxxopts::value<int>()->default_value("100"));
+        ("r,routes","# of routes to generate",
+        cxxopts::value<int>()->default_value("5"));
+
+    // # addresses per route
+    options.add_options()
+        ("a,addresses","# of addresses to generate per route",
+        cxxopts::value<int>()->default_value("5"));
+
     // coordinate space option
     options.add_options()
         ("s,space","defines x-y coordinate space",
@@ -144,10 +177,15 @@ int main(int argc, char **argv){
     }
 
     // perform n option operation
-    auto num_routes = result["n_routes"].as<int>();
+    auto num_adds = result["addresses"].as<int>();
+    auto num_routes = result["routes"].as<int>();
     auto space = result["space"].as<int>();
     auto keyword = result["primes"].as<std::string>();
     
+    std::vector<int> vec{};
+    for (int i = 0; i < num_adds; i++) {
+      generateAddress(keyword, space, vec).display();
+    }
     // do for loop 
         // try generateRoute, catch errors
         // each time, make route generation output
