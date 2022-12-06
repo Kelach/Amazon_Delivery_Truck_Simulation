aaf5fc5f3c8bc50a43ef82a3a014a9afc5c57205
ashtonvcole <ashtoncole1028@gmail.com>
2022-12-06 16:37:09 -0600

    Simulation almost done :D

--- src/experimental.cpp
+++ src/experimental.cpp
@@ -33,7 +33,6 @@ void optimize_routes(std::vector<Route>& routes) {
  * 
  * @note files must follow specifications to be read in correctly.
  * 
- * @param day_no current date
  * @param unfulfilled_orders_from file path to access unfulfilled orders from previous days
  * @param new_orders_from file path to access new orders from today
  * @param num_trucks number of trucks available on that day to divy up
@@ -42,7 +41,7 @@ void optimize_routes(std::vector<Route>& routes) {
  * 
  * @return number of undelivered orders in a day
 */
-int day(int day_no, string unfulfilled_orders_from, string new_orders_from, int num_trucks, double max_dist, Address hub) {
+int day(string unfulfilled_orders_from, string new_orders_from, int num_trucks, double max_dist, Address hub) {
     // Read in unfulfilled and new orders, combine into megalist
     std::vector<Address> unfulfilled_orders = AddressList::from_dat(unfulfilled_orders_from).get_vec();
     std::vector<Address> new_orders = AddressList::from_dat(new_orders_from).get_vec();
@@ -74,6 +73,10 @@ int day(int day_no, string unfulfilled_orders_from, string new_orders_from, int 
             unfulfilled_orders.push_back(routes.at(j).remove_least_priority());
         }
     }
+
+    // All leftover orders are the new unfulfilled orders, overwriting existing file
+    AddressList unfulfilled_orders_list(unfulfilled_orders);
+    unfulfilled_orders_list.to_dat(unfulfilled_orders_from);
     
     // Return number of unfulfilled orders
     return unfulfilled_orders.size();
@@ -85,19 +88,81 @@ int main() {
 
     // First scenario, varying distance
     int num_trucks1 = 5;
-    std::vector<double> max_dist1 = {250.0, 300.0, 350.0, 400.0, 450.0, 500.0, 550.0, 600.0, 650.0, 700.0};
+    std::vector<double> max_dist1s = {50.0, 55.0, 60.0, 65.0, 70.0, 75.0, 80.0, 85.0, 90.0, 95.0};
 
     // Second scenario, varying num_trucks
-    std::vector<int> num_trucks2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    double max_dist2 = 450.0;
+    std::vector<int> num_trucks2s = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    double max_dist2 = 75.0;
 
     // For both
     Address hub(0, 0, 0);
-    int day_max = 9;
+    int day_max = 90;
+
+    // File I/0
+    string dat_from = "..\\Experimental\\datain\\"; // Folder path to read data from
+    string dat_to = "..\\Experimental\\dataout\\"; // Folder path to read data to
+    std::ofstream data1; // Experimental data for experiment 1
+    std::ofstream data2; // Experimental data for experiment 2
 
     // Experiment 1: Varying Maximum Distance
+    std::cout << "Experiment 1" << '\n';
+    data1.open(dat_to + "experiment1_data.dat");
+    for (double max_dist1 : max_dist1s) {
+        std::cout << "Case max_dist = " << max_dist1 << '\n';
+        data1 << max_dist1 << " ";
+        for (int day_no = 1; day_no <= day_max; day_no++) {
+            data1 << day(
+                dat_from + "unfulfilled.dat",
+                dat_from + "day" + std::to_string(day_no) + ".dat",
+                num_trucks1,
+                max_dist1,
+                hub);
+            if (day_no < day_max) {
+                data1 << " ";
+                std::cout << "|";
+            }
+            else {
+                data1 << '\n';
+                std::cout << "|" << '\n';
+            }
+        }
+
+        // At end of each simulation, clear unfulfilled
+        std::ofstream unfulfilled;
+        unfulfilled.open(dat_from + "unfulfilled.dat");
+        unfulfilled.close();
+    }
+    data1.close();
 
     // Experiment 2: Varying Number of Trucks
+    std::cout << "Experiment 2" << '\n';
+    data2.open(dat_to + "experiment2_data.dat");
+    for (double num_trucks2 : num_trucks2s) {
+        std::cout << "Case num_trucks = " << num_trucks2 << '\n';
+        data2 << num_trucks2 << " ";
+        for (int day_no = 1; day_no <= day_max; day_no++) {
+            data2 << day(
+                dat_from + "day" + std::to_string(day_no) + ".dat",
+                dat_from + "unfulfilled.dat",
+                num_trucks2,
+                max_dist2,
+                hub);
+            if (day_no < day_max) {
+                data2 << " ";
+                std::cout << "|";
+            }
+            else {
+                data2 << '\n';
+                std::cout << "|" << '\n';
+            }
+        }
+
+        // At end of each simulation, clear unfulfilled
+        std::ofstream unfulfilled;
+        unfulfilled.open(dat_from + "unfulfilled.dat");
+        unfulfilled.close();
+    }
+    data2.close();
 
     // Stop timing simulation
     auto stop = std::chrono::high_resolution_clock::now();
---
+++ src/plotter.m
@@ -0,0 +1,5 @@
+% Delivery Truck Simulation
+% Plot graphs from experimental data
+
+data1 = readmatrix('../Experimental/dataout/experiment1_data.dat');
+data2 = readmatrix('../Experimental/dataout/experiment2_data.dat');
