f8ea831fdde4122ea81b7268a01068a4269b6bdf
Kelach <93161153+Kelach@users.noreply.github.com>
2022-12-07 21:21:50 -0600

    polished sections 2.2 2.3, included figures w/distances

--- src/tester.cpp
+++ src/tester.cpp
@@ -5,7 +5,32 @@
 
 #include "traveling_salesman.h" // imports TSP header file
 #include <iostream>             // imports standard library thingies
-using namespace TravelingSalesman; // we may not want to keep this
+#include <random>
+using namespace TravelingSalesman; 
+
+std::random_device rd; //obtain a random number hardware
+std::mt19937 gen(rd()); // seed the generator
+int random(int low, int high){
+    // std::cout <<"Low is: " << low << " High is: " << high << "\n";
+    std::uniform_int_distribution<> dist(low, high); // define range
+    return dist(gen); // generats random number
+}
+Route generateRoute(int space, int size){
+    Route newRoute( std::vector<Address>{}, Address(0, 0, 0));
+    for (int x = 0; x < size; x++){
+        int i = random(0, space);
+        int j = random(0, space);
+        int delivery_by = random(1, 7);
+        // only hub can have coordinates (0, 0)
+        while ((i+j) == 0){
+            j = random(0, space);
+            i = random(0, space);
+        }
+        newRoute.add_address(Address(i, j, delivery_by));
+
+    }
+        return newRoute;
+}
 
 int main() {
 
@@ -112,34 +137,76 @@ int main() {
     route2.display();
     std::cout << std::endl;
 
-    // Route r(std::vector<Address> {}, Address(0, 0, 0));
-    // r.add_address(Address(2, 0, 10));
-    // r.add_address(Address(2, 1, 10));
-    // r.add_address(Address(1, 2, 10));
-    // r.add_address(Address(0, 2, 10));
-
-    // Route s(std::vector<Address> {}, Address(0, 0, 0));
-    // s.add_address(Address(3, 1, 10));
-    // s.add_address(Address(3, 2, 10));
-    // s.add_address(Address(2, 3, 10));
-    // s.add_address(Address(1, 3, 10));
-    // std::cout << r.length() << " and "
-    //     << s.length() << "\n"; 
+ 
 
     route1.to_dat("..\\dataout\\demo_2_route1_swapped.dat");
     route1.to_tikz("..\\dataout\\demo_2_route1_swapped.tikz", "red", "black", "black", "-latex");
     route1.to_job("..\\dataout\\demo_2_route1_swapped.txt");
     route2.to_dat("..\\dataout\\demo_2_route2_swapped.dat");
     route2.to_tikz("..\\dataout\\demo_2_route2_swapped.tikz", "red", "gray", "gray", "-latex");
-    route1.to_job("..\\dataout\\demo_2_route2_swapped.txt");
+    route2.to_job("..\\dataout\\demo_2_route2_swapped.txt");
 
-    address_list.to_dat("..\\dataout\\demorw.dat");
-    std::cout << "\nWritten to file: ";
-    address_list.display();
-    AddressList from_file = AddressList::from_dat("..\\dataout\\demorw.dat");
-    std::cout << "\nRead from file: ";
-    from_file.display();
-    from_file.to_dat("..\\dataout\\demorw2.dat");
+    // address_list.to_dat("..\\dataout\\demorw.dat");
+    // std::cout << "\nWritten to file: ";
+    // address_list.display();
+    // AddressList from_file = AddressList::from_dat("..\\dataout\\demorw.dat");
+    // std::cout << "\nRead from file: ";
+    // from_file.display();
+    // from_file.to_dat("..\\dataout\\demorw2.dat");
+    
+    // Route pre_greedy = generateRoute(3, 5);
+    // std::cout << "previous dist: " << pre_greedy.length() << "\n";
+    // pre_greedy.to_tikz("..\\Report\\route_figures\\pre_greedy.tikz", "red", "gray", "black", "-latex");
+    // Route post_greedy = pre_greedy.greedy_route();
+    // std::cout << "new dist: " << post_greedy.length() << "\n";
+    // post_greedy.to_tikz("..\\Report\\route_figures\\post_greedy.tikz", "red", "green", "black", "-latex");
+    
+    // Route pre_opt2 = generateRoute(3, 5);
+    // std::cout << "\nprevious dist: " << pre_opt2.length() << "\n";
+    // pre_opt2.to_tikz("..\\Report\\route_figures\\pre_opt2.tikz", "red", "gray", "black", "-latex");
+    // Route post_opt2 = pre_opt2.greedy_route();
+    // std::cout << "new dist: " << post_opt2.length() << "\n";
+    // post_opt2.to_tikz("..\\Report\\route_figures\\post_opt2.tikz", "red", "green", "black", "-latex");
+     
+    // Route r1 = generateRoute(3, 8);
+    // std::cout << "\nprevious dist: " << r1.length() << "\n";
+    // r1.to_tikz("..\\Report\\route_figures\\r1_pre.tikz", "red", "gray", "black", "-latex");
 
+    // Route r2 = generateRoute(3, 6);
+    // std::cout << "\nprevious dist: " << r2.length() << "\n";
+    // r2.to_tikz("..\\Report\\route_figures\\r2_pre.tikz", "red", "gray", "black", "-latex");
+    
+    // r1.multi_opt2(r2);
+    // std::cout << "\nnew dist1: " << r1.length() << "\n";
+    // std::cout << "\nnew dist2: " << r2.length() << "\n";
+
+    // r1.to_tikz("..\\Report\\route_figures\\r1_post.tikz", "red", "gray", "black", "-latex");
+    // r2.to_tikz("..\\Report\\route_figures\\r2_post.tikz", "red", "gray", "black", "-latex");
+
+    Route r(std::vector<Address> {}, Address(0, 0, 0));
+    r.add_address(Address(2, 0, 10));
+    r.add_address(Address(3, 2, 10));
+    r.add_address(Address(2, 3, 10));
+    r.add_address(Address(0, 2, 10));
+
+    Route s(std::vector<Address> {}, Address(0, 0, 0));
+    s.add_address(Address(3, 1, 10));
+    s.add_address(Address(2, 1, 10));
+    s.add_address(Address(1, 2, 10));
+    s.add_address(Address(1, 3, 10));
+    std::cout << r.length() << " and "
+        << s.length() << "\n"; 
+    
+    s.display();
+    std::cout << "\n";
+    s.swap(r, 3, 4, 3, 4);
+    s.display();
+    // r.multi_opt2(s);
+    // s.to_tikz("..\\Report\\route_figures\\fig_5_1.tikz", "red", "gray", "green", "-latex");
+    // r.to_tikz("..\\Report\\route_figures\\fig_5_2.tikz", "red", "gray", "green", "-latex");
+    // std::cout << r.length() << " and "
+    //     << s.length() << "\n";
+
+    
     return 0;
 }
